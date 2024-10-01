2d70b2bca6f40fadefbfd2346632bea1a7b259c3
Khianne <Khianne@LAPTOP-9A1RGFEU>
2024-10-01 19:11:04 +0800

    Branch 1 ver. 1

--- src/activity_certifico_jalique_sevillano_1/Activity_Certifico_Jalique_Sevillano_1.java
+++ src/activity_certifico_jalique_sevillano_1/Activity_Certifico_Jalique_Sevillano_1.java
@@ -1,6 +1,4 @@
-
 package activity_certifico_jalique_sevillano_1;
-
 
 public class Activity_Certifico_Jalique_Sevillano_1 {
 
@@ -8,6 +6,17 @@ public class Activity_Certifico_Jalique_Sevillano_1 {
     public static void main(String[] args) {
       
         System.out.println("HELLO WORLD");
-        
+       int a,b,c;
+       a = 2; b =25; c = 100;
+      
+       int sum = a + b + c;
+        System.out.println(" Addition: "+sum);
+        int sub = c - b - a;
+        System.out.println(" Subtraction: "+sub);
+        int mul = a * b * c;
+        System.out.println(" Multiplication: "+mul);
+         float div = c / b / a;
+        System.out.println(" Division: "+div);
+       
     }   
 }
