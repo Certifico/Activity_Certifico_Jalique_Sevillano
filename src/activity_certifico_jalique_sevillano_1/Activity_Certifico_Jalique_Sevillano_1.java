11fb578d9c65f6e6f078728f62db010936661669
Khianne <Khianne@LAPTOP-9A1RGFEU>
2024-10-01 18:11:37 +0800

    12345

---
+++ src/activity_certifico_jalique_sevillano_1/Activity_Certifico_Jalique_Sevillano_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package activity_certifico_jalique_sevillano_1;
+
+/**
+ *
+ * @author Khianne
+ */
+public class Activity_Certifico_Jalique_Sevillano_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
