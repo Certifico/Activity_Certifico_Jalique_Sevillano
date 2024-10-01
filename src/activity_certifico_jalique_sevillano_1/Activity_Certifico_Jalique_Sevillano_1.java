70e1a05457b97191b2b52e7114456ec26021b30f
USER <USER@GRAPHENE>
2024-10-01 18:28:08 +0800

    Version2

--- src/activity_certifico_jalique_sevillano_1/Activity_Certifico_Jalique_Sevillano_1.java
+++ src/activity_certifico_jalique_sevillano_1/Activity_Certifico_Jalique_Sevillano_1.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package activity_certifico_jalique_sevillano_1;
 
-/**
- *
- * @author Khianne
- */
+
 public class Activity_Certifico_Jalique_Sevillano_1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+      
+        System.out.println("HELLO WORLD");
+        
+    }   
 }
